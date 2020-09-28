825bb747b0064fcdc8845d84d51cf5e84c237015
cxcd <arbrihalili@gmail.com>
2020-09-28 18:21:24 -0400

    added main function skeleton

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("Hello World")
+#import os
+
+def main():
+    print("Hello world")
+
+if __name__ == '__main__':
+    main()
+    #main(train_path=os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data.txt')
