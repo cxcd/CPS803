c315a63e71f395d20ceec56b5d127a5b708b8295
cxcd <arbrihalili@gmail.com>
2020-10-09 16:52:13 -0400

    example data

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-#import os
+import os
 from numba import jit
 import numpy as np
+import pretty_midi
 
 x = np.arange(100).reshape(10, 10)
 
@@ -12,9 +13,30 @@ def go_fast(a): # Function is compiled to machine code when called the first tim
     return a + trace              # Numba likes NumPy broadcasting
 
 
-def main():
-    print(go_fast(x))
+def read_midi(midi_path):
+    midi_data = pretty_midi.PrettyMIDI('example.midi')
+    for i in midi_data.instruments[0].notes:
+        print(i)
+
+def make_midi(output_path):
+    # Create the output structure
+    output = pretty_midi.PrettyMIDI()
+    # Create the instrument program and instrument
+    piano_program = pretty_midi.instrument_name_to_program('Acoustic Grand Piano')
+    piano = pretty_midi.Instrument(program=piano_program)
+    # Simple list of notes - C minor
+    notes = np.array([
+        pretty_midi.Note(velocity=100, pitch=pretty_midi.note_name_to_number('C5'), start=0, end=1),
+        pretty_midi.Note(velocity=100, pitch=pretty_midi.note_name_to_number('Eb5'), start=1, end=2),
+        pretty_midi.Note(velocity=100, pitch=pretty_midi.note_name_to_number('G5'), start=2, end=3)
+        ])
+    # Set the piano notes to the list of notes
+    piano.notes = notes
+    # Give the output our instrument
+    output.instruments.append(piano)
+    # Write the output
+    output.write(output_path)
 
 if __name__ == '__main__':
-    main()
-    #main(train_path=os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data.txt')
+    #read_midi(midi_path=os.path.join(os.path.dirname(os.path.abspath(__file__)), 'example.midi'))
+    make_midi('piano-C-chord.mid')
