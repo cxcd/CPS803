2d158e4e756a444e1f3c488cbc0c05c4992e1686
cxcd <arbrihalili@gmail.com>
2020-10-26 16:41:02 -0400

    Update transformer.py

--- transformer.py
+++ transformer.py
@@ -1,182 +1,136 @@
-# ML
 import math
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-# Util
-import time
 
-# Hold information to inject about the relative or absolute position of the tokens in the sequence
-class PositionalEncoding(nn.Module):
-    def __init__(self, d_model, dropout=0.1, max_len=5000):
-        super(PositionalEncoding, self).__init__()
-        self.dropout = nn.Dropout(p=dropout)
+def d(tensor=None):
+    """ Return best available device """
+    if tensor is None:
+        return 'cuda' if torch.cuda.is_available() else 'cpu'
+    return 'cuda' if tensor.is_cuda else 'cpu'
 
-        pe = torch.zeros(max_len, d_model)
-        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
-        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
-        pe[:, 0::2] = torch.sin(position * div_term)
-        pe[:, 1::2] = torch.cos(position * div_term)
-        pe = pe.unsqueeze(0).transpose(0, 1)
-        self.register_buffer('pe', pe)
+def mask(matrices, mask_val, mask_diagonal=True):
+    """ Mask all values in place of given batch of matrices. Upper triangle becomes mask_val """
+    b, h, w = matrices.size()
+    indices = torch.triu_indices(h, w, offset=0 if mask_diagonal else 1)
+    matrices[:, indices[0], indices[1]] = mask_val
+
+class SelfAttention(nn.Module):
+    """ Multi-headed, scaled dot-product self attention """
+    def __init__(self, emb, heads=8, mask=False):
+        super().__init__()
+        self.emb = emb
+        self.heads = heads
+        self.mask = mask
+        # q, k, v
+        # Map query against set of keys to values
+        self.to_queries = nn.Linear(emb, emb * heads, bias=False)
+        self.to_keys = nn.Linear(emb, emb * heads, bias=False)
+        self.to_values = nn.Linear(emb, emb * heads, bias=False)
+        # Unify output of heads to a single emb-vector
+        self.unify_heads = nn.Linear(heads * emb, emb)
+    
+    def forward(self, x):
+        b, t, e = x.size()
+        h = self.heads
+        # Init
+        queries = self.to_queries(x).view(b, t, h, e)
+        keys = self.to_keys(x).view(b, t, h, e)
+        values = self.to_values(x).view(b, t, h, e)
+        # Fold head into batch dimension
+        queries = queries.transpose(1, 2).contiguous().view(b * h, t, e)
+        keys = keys.transpose(1, 2).contiguous().view(b * h, t, e)
+        values = values.transpose(1, 2).contiguous().view(b * h, t, e)
+        # Get dot product
+        dot = torch.bmm(queries, keys.transpose(1, 2))
+        # Dot contains b * h, t x t matrices with raw self attention logits
+        dot = dot / math.sqrt(e)
+        # Mask lower half of the dot matrix incl diagonal
+        if self.mask:
+            mask(dot, maskval=float('-inf'), mask_diagonal=False)
+        # Dot contains row-wise self-attention probaiblities
+        dot = F.softmax(dot, dim=2)
+        # Remove NaN
+        if self.mask == 'first':
+            dot = dot.cone()
+            dot[:, :1 :] = 0.0
+        # Apply self attention to values
+        out = torch.bmm(dot, values).view(b, h, t, e)
+        # Swap h, t
+        out = out.transpose(1, 2).contiguous().view(b, t, h * e)
+        # Unify heads
+        return self.unify_heads(out)
+
+class TransformerBlock(nn.Module):
+    """ Transformer block: attn + norm -> ff + norm """
+    def __init__(self, emb, n_heads, mask, seq_length, ff_hidden_mult=4, dropout=0.0):
+        super().__init__()
+        # Self attention layer
+        self.attention = SelfAttention(emb, n_heads=n_heads)
+        # Layer normalization
+        self.norm1 = nn.LayerNorm(emb)
+        self.norm2 = nn.LayerNorm(emb)
+        # Feed-forward layer
+        self.ff = nn.Sequential(
+            nn.Linear(emb, ff_hidden_mult * emb),
+            nn.ReLU(),
+            nn.Linear(ff_hidden_mult * emb, emb)
+        )
+        self.dropout = nn.Dropout(dropout)
+    
+    def forward(self, x):
+        # Attention
+        attended = self.attention(x)
+        # First norm layer
+        x = self.norm1(attended + x)
+        # Dropout
+        x = self.dropout(x)
+        # Feed-forward layer
+        fed_forward = self.ff(x)
+        # Second norm layer
+        x = self.norm2(fed_forward + x)
+        # Dropout
+        x = self.dropout(x)
+        # Complete
+        return x
+
+class Transformer(nn.Module):
+    """ Autoregressive transformer model """
+    def __init__(self, emb, heads, depth, seq_length, n_tokens):
+        super().__init__()
+        # number of tokens
+        self.n_tokens = n_tokens
+        # Token Embedding
+        self.token_embedding = nn.Embedding(embedding_dim=emb, num_embeddings=n_tokens)
+        # Positional embedding
+        self.pos_embedding = nn.Embedding(embedding_dim=emb, num_embeddings=seq_length)
+        # Linearly transform embeddings to unify
+        self.unify_embeddings = nn.Linear(2 * emb, emb)
+        # Depth of transformer
+        t_blocks = []
+        for i in range(depth):
+            t_blocks.append(
+                TransformerBlock(
+                    emb=emb,
+                    heads=heads,
+                    seq_length=seq_length,
+                    mask=True
+                )
+            )
+        self.t_blocks = nn.Sequential(*t_blocks)
+        # To probabilities
+        self.to_probs = nn.Linear(emb, n_tokens)
 
     def forward(self, x):
-        x = x + self.pe[:x.size(0), :]
-        return self.dropout(x)
+        tokens = self.token_embedding(x)
+        b, t, e = tokens.size()
+        # Get positional embeddings of the batch
+        positions = self.pos_embedding(torch.arrange(t, device=d()))[None, :, :].expand(b, t, e)
+        # Unify embeddings
+        x = self.unify_embeddings(torch.cat((tokens, positions), dim=2).view(-1, 2 * e)).view(b, t, e)
+        # Run the batch through transformer blocks
+        x = self.t_blocks(x)
+        x = self.to_probs(x.view(b * t, e)).view(b, t, self.n_tokens)
+        # Predicted log probability for each token based on preceding tokens
+        return F.log_softmax(x, dim=2)
 
-class TransformerModel(nn.Module):
-    # Initialize
-    def __init__(self, ntoken, ninp, nhead, nhid, nlayers, dropout=0.5):
-        super(TransformerModel, self).__init__()
-        from torch.nn import TransformerEncoder, TransformerEncoderLayer
-        self.model_type = 'Transformer'
-        self.src_mask = None
-        self.pos_encoder = PositionalEncoding(ninp, dropout)
-        encoder_layers = TransformerEncoderLayer(ninp, nhead, nhid, dropout)
-        self.transformer_encoder = TransformerEncoder(encoder_layers, nlayers)
-        self.encoder = nn.Embedding(ntoken, ninp)
-        self.ninp = ninp
-        self.decoder = nn.Linear(ninp, ntoken)
-
-        self.init_weights()
-    
-    # Create empty square attention mask
-    def _generate_square_subsequent_mask(self, sz):
-        mask = (torch.triu(torch.ones(sz, sz)) == 1).transpose(0, 1)
-        mask = mask.float().masked_fill(mask == 0, float('-inf')).masked_fill(mask == 1, float(0.0))
-        return mask
-    
-    # Initialize weights
-    def init_weights(self):
-        initrange = 0.1
-        self.encoder.weight.data.uniform_(-initrange, initrange)
-        self.decoder.bias.data.zero_()
-        self.decoder.weight.data.uniform_(-initrange, initrange)
-
-    def forward(self, src):
-        # If there is no mask, make a mask
-        if self.src_mask is None or self.src_mask.size(0) != src.size(0):
-            device = src.device
-            mask = self._generate_square_subsequent_mask(src.size(0)).to(device)
-            self.src_mask = mask
-        # Encode, decode
-        src = self.encoder(src) * math.sqrt(self.ninp)
-        src = self.pos_encoder(src)
-        output = self.transformer_encoder(src, self.src_mask)
-        output = self.decoder(output)
-        return output
-
-class Generator():
-    '''
-    Initialize the model.
-    ntokens = the size of the vocabulary
-    emsize = embedding dimensions
-    nhid = dimensions of feeedforward network model in nn.TransformerEncoder
-    nlayers = number of nn.TransformerEncoderLayer in nn.TransformerEncoder
-    nhead = number of heads in the multheadattention models
-    dropout = the dropout value
-    '''
-    def __init__(self, train_data, valid_data, test_data, ntokens, emsize=200, nhid=200, nlayers=2, nhead=2, dropout=0.2, bptt=35): 
-        self.bptt = bptt
-        self.train_data = train_data
-        self.valid_data = valid_data
-        self.test_data = test_data
-        self.ntokens = ntokens
-        # Find the device to run the model on
-        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        # Construct the model
-        self.model = TransformerModel(self.ntokens, emsize, nhead, nhid, nlayers, dropout).to(device)
-        # Get the loss
-        self.criterion = nn.CrossEntropyLoss()
-        self.lr = 5.0 # learning rate
-        self.optimizer = torch.optim.SGD(model.parameters(), lr=lr)
-        self.scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 1.0, gamma=0.95)
-
-    '''
-    Generate input and target sequence for the transformer model.
-    Subdivide source data into chunks of length 'bptt'
-    '''
-    def get_batch(self, i, source):
-        seq_len = min(self.bptt, len(source) - 1 - i)
-        data = source[i:i + seq_len]
-        target = source[i + 1 : i + 1 + seq_len].view(-1)
-        return data, target
-    
-    '''
-    Train the model.
-    CrossEntropyLoss is applied to trakc the loss and SGD is the optimizer. 
-    The initial learning rate is .50 and StepLR is applied to adjust the learing rate through epochs.
-    clip_grad_norm scales all the gradients to prevent exploding.
-    '''
-    def train(self, epoch, log_interval=200):
-        # Turn on the train mode of the model
-        self.model.train()
-        # Init values
-        total_loss = 0
-        start_time = time.time()
-        # For the data
-        for batch, i in enumerate(range(0, self.train_data.size(0) - 1, self.bptt)):
-            # Get a batch
-            data, targets = get_batch(i, self.train_data)
-            # Reset the optimizer to stop accumulating gradient
-            optimizer.zero_grad()
-            # Get output
-            output = model(data)
-            # Get loss
-            loss = criterion(output.view(-1, self.ntokens), targets)
-            loss.backward()
-            # Clip to prevent exploding
-            torch.nn.utils.clip_grad_norm_(model.parameters(), 0.5)
-            # Step optimizer
-            optimizer.step()
-            # Accumulate loss
-            total_loss += loss.item()
-            # When an interval of batches is complete, log the progress
-            if batch % log_interval == 0 and batch > 0:
-                curr_loss = total_loss / log_interval
-                elapsed = time.time() - start_time
-                print('| epoch {:3d} | {:5d}/{:5d} batches | '
-                  'lr {:02.2f} | ms/batch {:5.2f} | '
-                  'loss {:5.2f} | ppl {:8.2f}'.format(
-                    epoch, batch, len(train_data) // bptt, scheduler.get_lr()[0],
-                    elapsed * 1000 / log_interval,
-                    curr_loss, math.exp(curr_loss)))
-                total_loss = 0
-                start_time = time.time()
-
-    def evaluate(self, data_source):
-        self.model.eval()
-        total_loss = 0
-        with torch.no_grad():
-            for i in range (0, data_source.size(0) - 1, self.bptt):
-                data, targets = get_batch(data_source, i)
-                output = model(data)
-                output_flat = output.view(-1, self.ntokens)
-                total_loss += len(data) * self.criterion(output_flat, targets).item()
-        return total_loss / (len(data_source) - 1)
-
-    def run(self, best_val_loss=float("inf"), epochs=3, best_model=None):
-        # Loop over epochs, save the model if the validation loss is the best we've seen, adjust learning rate after each epoch
-        for epoch in range(1, epochs + 1):
-            epoch_start_time = time.time()
-            train(epoch)
-            val_loss = evaluate(model, self.valid_data)
-            print('-' * 89)
-            print('| end of epoch {:3d} | time: {:5.2f}s | valid loss {:5.2f} | '
-                'valid ppl {:8.2f}'.format(epoch, (time.time() - epoch_start_time),
-                                            val_loss, math.exp(val_loss)))
-            print('-' * 89)
-
-        if val_loss < best_val_loss:
-            best_val_loss = val_loss
-            best_model = model
-        scheduler.step()
-        
-        # Evaluate model with the test dataset, apply the best model to check the results with the dest dataset
-        test_loss = evaluate(best_model, test_data)
-        print('=' * 89)
-        print('| End of training | test loss {:5.2f} | test ppl {:8.2f}'.format(
-            test_loss, math.exp(test_loss)))
-        print('=' * 89)
-        # Save model
-        # torch.save(best_model.state_dict(), "model")
